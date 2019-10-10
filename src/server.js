9cbeadb72aeac6ad3acb84f105127c4eeb0fcec0
jhow98 <jhonatan@fho.edu.br>
2019-10-09 21:15:06 -0300

    config server

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb//mongodb://127.0.0.1:27017/mongodb', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
