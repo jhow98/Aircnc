7f6c5f43dd9f060865644aba2a8d282710951313
jhow98 <jhonatan@fho.edu.br>
2019-10-10 00:23:17 -0300

    set app url

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -16,7 +16,7 @@ const SpotSchema = new mongoose.Schema({
 });
 
 SpotSchema.virtual('thumbnail_url').get(function(){
-    return `https://aircnc-rocketseat-backend.herokuapp.com/files/${this.thumbnail}`
+    return `${process.env.APP_URL}/files/${this.thumbnail}`
 })
 
 module.exports = mongoose.model('Spot', SpotSchema);
